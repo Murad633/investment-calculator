8a4d469538ad9b14b7528c902299938a179a3e83
Murad Israfilov <muradisrafilov542@gmail.com>
2024-10-06 01:51:40 +0400

    added header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export const Header = () => {
+  return (
+    <header id="header">
+      <img src="./assets/investment-calculator-logo.png" alt="" />
+      <h1>Investment Calculator</h1>
+    </header>
+  );
+};
